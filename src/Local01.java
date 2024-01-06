e2606efb8e4e556583d325f032691db750a21a54
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-06 21:57:11 +0100

    Local01 ve Local02 eklendi

---
+++ src/Local01.java
@@ -0,0 +1,7 @@
+public class Local01 {
+    public static void main(String[] args) {
+        System.out.println("Git Test");
+        System.out.println("Yeni satir eklendi !! ");
+        System.out.println("Yeni test");
+    }
+}
---
+++ src/Local02.java
@@ -0,0 +1,7 @@
+public class Local02 {
+    public static void main(String[] args) {
+        System.out.println("Test1");
+        System.out.println("Test2");
+
+    }
+}
