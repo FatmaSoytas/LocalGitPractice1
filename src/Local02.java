4a7e718d19dcad0e8c675462be1cc071908a7b83
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-08 00:23:56 +0100

    Local02 de test3  eklendi

--- src/Local02.java
+++ src/Local02.java
@@ -2,6 +2,7 @@ public class Local02 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test2");
+        System.out.println("Test3");
 
     }
 }
